ded52574b453292e508a027b7f8d46dfe8d960fa
Ali Gholipour <ahmadiali042@gmail.com>
2022-11-14 22:14:13 +0330

    Fanout exchange sample added

---
+++ Basic/FanoutExchange/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.VirtualHost = "/";
+factory.Port = 5672;
+factory.UserName = "guest";
+factory.Password = "guest";
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "sport_news", type: "fanout", true, false);
+
+channel.QueueDeclare(queue: "QueueA", true, false, false);
+channel.QueueDeclare(queue: "QueueB", true, false, false);
+channel.QueueDeclare(queue: "QueueC", true, false, false);
+
+channel.QueueBind(queue: "QueueA", exchange: "sport_news", routingKey: "");
+channel.QueueBind(queue: "QueueB", exchange: "sport_news", routingKey: "");
+channel.QueueBind(queue: "QueueC", exchange: "sport_news", routingKey: "");
+
+channel.BasicPublish(exchange: "sport_news", routingKey: "");
+
