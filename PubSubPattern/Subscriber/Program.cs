1ac60cd43f73b63e9239883f4ca42a4c9679b99b
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-21 23:02:31 +0330

    Subscriber completed

--- PubSubPattern/Subscriber/Program.cs
+++ PubSubPattern/Subscriber/Program.cs
@@ -1,1 +1,34 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+ConnectionFactory factory = new ConnectionFactory();
+factory.HostName = "localhost";
+factory.VirtualHost = "/";
+factory.Port = 5672;
+factory.UserName = "guest";
+factory.Password = "guest";
+
+var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+Console.WriteLine("Enter the queue name:");
+
+string queueName = Console.ReadLine();
+
+channel.ExchangeDeclare("publisher", "fanout", false, false);
+channel.QueueDeclare(queue: queueName, true, false, false);
+channel.QueueBind(queue: queueName, exchange: "publisher", routingKey: "");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, arg) =>
+{
+    string message = Encoding.UTF8.GetString(arg.Body.ToArray());
+    channel.BasicAck(arg.DeliveryTag, false);
+    Console.WriteLine($"Subscriber: [{queueName}] message: {message}");
+};
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadKey();
